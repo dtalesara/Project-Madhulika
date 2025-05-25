68052de79c3232e0b5dddd1243b0f93414eab230
dtalesara <diyatalesara@hotmail.com>
2025-05-25 19:07:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 
+
 import streamlit as st
 
-st.title("📚 Support Resources")
+st.set_page_config(page_title="Project Madhulika", page_icon="🌙")
+
+# Title and image
+st.title("🌙 Project Madhulika")
+st.image("moon_image.jpg", caption="Empowering women through resources", use_column_width=True)
 
 resources = {
     "📞 Emergency Services": [
